743b5d687621f8571792d8ca62062410d146799a
임성우 <seongwoorang@naver.com>
2024-06-25 17:10:52 +0900

    :sparkles: #51 - post repository 팔로우 사용자 게시물 가져오기 기능 테스트 코드

--- src/test/kotlin/com/teamsparta/jobtopia/domain/post/repository/PostRepositoryTest.kt
+++ src/test/kotlin/com/teamsparta/jobtopia/domain/post/repository/PostRepositoryTest.kt
@@ -6,6 +6,7 @@ import com.teamsparta.jobtopia.domain.users.model.Profile
 import com.teamsparta.jobtopia.domain.users.model.Users
 import com.teamsparta.jobtopia.domain.users.repository.UserRepository
 import com.teamsparta.jobtopia.infra.querydsl.QueryDslSupport
+import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -80,6 +81,63 @@ class PostRepositoryTest {
         // THEN
         result.content.size shouldBe 4
         result2.content.size shouldBe 1
+    }
+
+    @Test
+    fun `팔로우하는 사용자의 게시물 가져오기`() {
+        // GIVEN
+        val profile1 = Profile(nickname = "nickname1", description = "description1")
+        val user1 = Users(userName = "user1", password = "password1", profile = profile1)
+        userRepository.save(user1)
+
+        val profile2 = Profile(nickname = "nickname2", description = "description2")
+        val user2 = Users(userName = "user2", password = "password2", profile = profile2)
+        userRepository.save(user2)
+
+        val postList = listOf(
+            Post(
+                title = "title1",
+                content = "content1",
+                users = user1,
+                updatedAt = LocalDateTime.now(),
+                deletedAt = LocalDateTime.now(),
+                files = ""
+            ),
+            Post(
+                title = "title2",
+                content = "content2",
+                users = user1,
+                updatedAt = LocalDateTime.now(),
+                deletedAt = LocalDateTime.now(),
+                files = ""
+            ),
+            Post(
+                title = "title3",
+                content = "content3",
+                users = user2,
+                updatedAt = LocalDateTime.now(),
+                deletedAt = LocalDateTime.now(),
+                files = ""
+            ),
+            Post(
+                title = "title4",
+                content = "content4",
+                users = user2,
+                updatedAt = LocalDateTime.now(),
+                deletedAt = LocalDateTime.now(),
+                files = ""
+            )
+        )
+        postRepository.saveAll(postList)
+
+        val userIds = listOf(user1.id!!, user2.id!!)
+        val pageable = PageRequest.of(0, 10)
+
+        // WHEN
+        val result = postRepository.getFollowingUserPosts(userIds, pageable)
+
+        // THEN
+        result.content shouldContainExactly postList
     }
 
 }
